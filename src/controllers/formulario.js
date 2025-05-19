575b59bf5cb693bb5d6b2f2537b359c271934eb2
Victor hugo <victr.hf@gmail.com>
2025-05-19 19:57:58 -0300

    first commit

---
+++ src/controllers/formulario.js
@@ -0,0 +1,9 @@
+function index (req, res){
+    res.json('formulario');
+}
+
+function store (req, res){
+    
+}
+
+module.exports = { index, store };
---
+++ src/routes/formulario.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const formularioController = require('../controllers/formulario');
+
+router.get('/', formularioController.index);
+router.post('/', formularioController.store);
+
+
+module.exports = router;
