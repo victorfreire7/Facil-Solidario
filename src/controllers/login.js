dd48103b442e6c7b0d3d396be70ced41e2890892
Victor hugo <victr.hf@gmail.com>
2025-05-19 18:52:15 -0300

    first commit

---
+++ src/controllers/login.js
@@ -0,0 +1,9 @@
+function index (req, res){
+    res.json('login');
+}
+
+function store (req, res){
+    
+}
+
+module.exports = { index, store };
---
+++ src/routes/login.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const loginController = require('../controllers/login');
+
+router.get('/', loginController.index);
+router.post('/', loginController.store);
+
+
+module.exports = router;
