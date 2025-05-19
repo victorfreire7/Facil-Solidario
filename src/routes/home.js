d3695553bcaeffc8dd0699c0d1d44cebc4d0efd5
Victor hugo <victr.hf@gmail.com>
2025-05-19 18:49:06 -0300

    minimal detail

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home');
 
 router.get('/', homeController.index);
-router.post('/', homeController.index);
+router.post('/', homeController.store);
 
 
 module.exports = router;
