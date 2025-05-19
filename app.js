e669cb5ca0f93a32b579155edf7385aaaae41cca
Victor hugo <victr.hf@gmail.com>
2025-05-19 19:58:11 -0300

    add formulario route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const pontoscoletaRoute = require('./src/routes/pontoscoleta');
 const politicasRoute = require('./src/routes/politicas');
 const cadastrarRoute = require('./src/routes/cadastrar');
 const loginRoute = require('./src/routes/login');
+const formularioRoute = require('./src/routes/formulario');
 
 
 
@@ -28,7 +29,7 @@ class App {
         this.app.use('/politicas-de-privacidade', politicasRoute);
         this.app.use('/sign-up', cadastrarRoute);
         this.app.use('/sign-in', loginRoute);
-
+        this.app.use('/formulario-doacao', formularioRoute);
     }
 }
 
