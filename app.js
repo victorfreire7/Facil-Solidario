fbc18d759f39020f42f1676f2c3b2d14250ee8b4
Victor hugo <victr.hf@gmail.com>
2025-05-19 18:38:06 -0300

    add politicas route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const homeRoute = require('./src/routes/home');
 const quemsomosRoute = require('./src/routes/quemsomos');
 const pontoscoletaRoute = require('./src/routes/pontoscoleta');
+const politicasRoute = require('./src/routes/politicas');
 
 
 class App {
@@ -20,6 +21,7 @@ class App {
         this.app.use('/', homeRoute);
         this.app.use('/quem-somos', quemsomosRoute);
         this.app.use('/pontos-de-coleta', pontoscoletaRoute);
+        this.app.use('/politicas-de-privacidade', politicasRoute);
     }
 }
 
