58bb82f503c090d7303604a27162f90f954de840
Victor hugo <victr.hf@gmail.com>
2025-05-23 21:05:49 -0300

    create model

--- src/models/doacao.js
+++ src/models/doacao.js
@@ -1,3 +1,41 @@
+const { randomUUID } = require('node:crypto'); 
 const { Sequelize } = require('sequelize');
 const db = require('../db');
 
+module.exports = () => {
+    const Doacao = db.define('doacao', {
+        id_doacao: {
+            type: Sequelize.STRING,
+            defaultValue: randomUUID(),
+            primaryKey: true,
+            allowNull: false
+        },
+    
+        tipo: {
+            type: Sequelize.STRING,
+            defaultValue: '',
+            allowNull: false,
+            validate: {
+                max: 50
+            }
+        },
+    
+        quantidade: {
+            type: Sequelize.INTEGER,
+            defaultValue: '',
+            allowNull: false,
+            validate: {
+                max: 50
+            }
+        },
+    
+        entregue: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: false,
+        },
+    
+    
+    });
+
+    return Doacao; //TA DANDO ERRO
+}
