c0c54851a5be0efa293b13e36d5500c40a522025
Victor hugo <victr.hf@gmail.com>
2025-05-23 23:31:43 -0300

    fixing erro

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -2,12 +2,18 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 module.exports = (req, res, next) => {
-    const token = req.headers['Authorization'];
-    if(!token) return res.status(401).json({auth:false, message: 'token nao providenciado'});
-
+    
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    
+    if (!token) {
+        return res.status(401).json({ auth: false, message: 'Token não providenciado' });
+    }
     jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-        if(err) return res.status(500).json({auth:false, message:'erro na authenticaçao'});
-
+        if (err) {
+            return res.status(500).json({ auth: false, message: 'Erro na autenticação' });
+        }
+        
+        req.userId = decoded.id; 
         next();
-    })
+    });
 }
