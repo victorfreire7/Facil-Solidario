b58164c5c4c874b48475803a3fa63002d5db6237
Victor <victr.hf@gmail>
2025-05-26 09:31:21 -0300

    initial commit

---
+++ src/models/admin.js
@@ -0,0 +1,48 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../db');
+const doacaoRepository = require('./doacao');
+const bcryptjs = require('bcryptjs');
+
+db.addHook('beforeSave', async user => {
+    if (user.senha) {
+        user.senha_hash = await bcryptjs.hash(user.senha, 8);
+    }
+})
+
+const Admin = db.define('admin', {
+    id_admin: {
+        type: DataTypes.STRING,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+    },
+
+    login: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: '',
+        unique: true,
+    },
+
+    senha_hash: {
+        type: DataTypes.STRING,
+        defaultValue: ''
+    },
+
+    senha: {
+        type: Sequelize.VIRTUAL,
+        allowNull: false,
+        defaultValue: '',
+        validate: {
+            len: {
+                args: [8, 50]
+            }
+        }
+    }
+
+});
+
+Admin.hasMany(doacaoRepository);
+doacaoRepository.belongsTo(Admin);
+
+module.exports = Admin;
