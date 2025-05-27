7b6bbbf9e85573e22e4ce70d8e0a0b03f137bbed
Victor hugo <victr.hf@gmail.com>
2025-05-26 23:22:38 -0300

    fix session

--- src/middlewares/adminloginRequired.js
+++ src/middlewares/adminloginRequired.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = (req, res, next) => {
-    if(!req.session.user){
+    if(!req.session.admin){
         res.json('login necessario');
         res.redirect('/');
     }
