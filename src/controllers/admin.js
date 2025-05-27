15e5074abaccebdce75e644102da23b59e5d44a1
Victor hugo <victr.hf@gmail.com>
2025-05-26 23:16:34 -0300

    first commit

---
+++ src/controllers/admin.js
@@ -0,0 +1,16 @@
+async function index(){
+
+}
+
+async function store(){
+    
+}
+
+async function show(){
+    
+}
+
+async function update(){
+}
+
+module.exports = { index, store, show, update }
