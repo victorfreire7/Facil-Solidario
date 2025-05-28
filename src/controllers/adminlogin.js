468b44a4de46a874d8778c766b1121c4a44bfc67
Victor hugo <victr.hf@gmail.com>
2025-05-27 23:55:06 -0300

    add random login acess for admin user

--- src/controllers/adminlogin.js
+++ src/controllers/adminlogin.js
@@ -1,8 +1,49 @@
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+
 const adminRepository = require('../models/admin');
+const randomStringGenerator = require('random-string-generator');
 const bcryptjs = require('bcryptjs');
 
+const dayLogin = randomStringGenerator(6); // gera uma string aleátoria de 6 caracteres.
+const dayPassword = randomStringGenerator(25); // faço o mesmo aqui
+
 async function index(req, res) {
-    // create();
+    const admins = await adminRepository.findOne(); //guardo dentro de uma constante os valores da tabela admin
+
+    if(!admins){ // caso nao exista um admin ainda, eu vou criar um.
+        await adminRepository.create({
+            login: dayLogin, // seto como valor de login, a string aleatoria criada anteriormente.   
+            senha_hash: await bcryptjs.hash(dayPassword, 8) // faço o mesmo com a senha.
+        })
+    } else { // caso já exista um admin, eu mudo os valores dele pros novos
+        await admins.update({
+            login: dayLogin,
+            senha_hash: await bcryptjs.hash(dayPassword, 8)
+        })
+    }
+    
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'solidariofacil@gmail.com',
+        from: 'solidariofacil@gmail.com',
+        subject: `LOGIN DE ADMINISTRADOR DO DIA.`,
+        text: 
+        `
+            Seu login diário para entrar como ADMINISTRADOR no sistemas do Fácil Solidário LTDA. é:
+
+            \n \n \n
+
+            LOGIN: ${dayLogin}   
+            SENHA: ${dayPassword}
+
+            \n \n \n
+
+            Tenha um bom serviço!
+        `
+    };
+    sgMail.send(msg); // envio no E-mail, o login e a senha do dia para acesso do admin
+
     res.json('login do admin');
 }
 
@@ -37,11 +78,11 @@ async function store(req, res) {
     }
 }
 
-async function create() {
-    await adminRepository.create({
-        login: "admin",
-        senha_hash: await bcryptjs.hash("admin123", 8)
-    })
-}
+// async function create() {
+//     await adminRepository.create({
+//         login: "admin",
+//         senha_hash: await bcryptjs.hash("admin123", 8)
+//     })
+// }
 
 module.exports = { index, store }
