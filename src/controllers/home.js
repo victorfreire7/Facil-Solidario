8b413a3c7a00f12f3677273e8208709fe75ae521
Victor hugo <victr.hf@gmail.com>
2025-05-27 22:34:33 -0300

    add email sender in main route

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
+
 function index (req, res){
     res.json('hello world!');
 }
 
-function store (req, res){
-    
+async function store (req, res){
+    try {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: 'solidariofacil@gmail.com',
+            from: 'solidariofacil@gmail.com',
+            subject: `nome: ${req.body.nome} email: ${req.body.email}`,
+            text: req.body.texto,
+        };
+        await sgMail.send(msg).then((result) => {
+            res.json(result);
+        })
+    } catch (error) {
+       res.json(error) 
+    }
 }
 
 module.exports = { index, store };
