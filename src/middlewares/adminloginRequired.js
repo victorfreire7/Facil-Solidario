f0ea462499a3b961e03ef4a4ec2d0972f9b1312f
Victor hugo <victr.hf@gmail.com>
2025-05-30 19:33:37 -0300

    documentation

--- src/middlewares/adminloginRequired.js
+++ src/middlewares/adminloginRequired.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const adminRepository = require('../models/admin');
 
-module.exports = async function (req, res, next) { // MUDEI AQUI, FIZ NAO SER MAIS UA ARROW FUNCITON
+module.exports = async function (req, res, next) {
     if(!req.session.admin){
         res.json('login necessario');
         res.redirect('/');
@@ -9,7 +9,7 @@ module.exports = async function (req, res, next) { // MUDEI AQUI, FIZ NAO SER MA
 
     await adminRepository.findOne()
     .then((result) => {
-        if(result.login != req.session.admin.login){ // verifico se as informações do BD coincidem.
+        if(result.login != req.session.admin.login){ // verifico se as informações do BD coincidem com as da sessao.
             res.json('redirecionando pra pag 404');
         }
     })
