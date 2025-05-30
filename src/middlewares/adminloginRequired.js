c7654df2460e29ad491d679169b44ca3cb6a5091
Victor hugo <victr.hf@gmail.com>
2025-05-30 19:32:58 -0300

    logic correction

--- src/middlewares/adminloginRequired.js
+++ src/middlewares/adminloginRequired.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const bcryptjs = require('bcryptjs');
 const adminRepository = require('../models/admin');
 
 module.exports = async function (req, res, next) { // MUDEI AQUI, FIZ NAO SER MAIS UA ARROW FUNCITON
@@ -8,17 +7,11 @@ module.exports = async function (req, res, next) { // MUDEI AQUI, FIZ NAO SER MA
         res.redirect('/');
     }
 
-    const admin = await adminRepository.findByPk(req.session.admin.id_admin);
-
-    console.log(admin.dataValues.senha_hash);
-    console.log('-------------------')
-    console.log(req.session.admin.senha_hash)
-
-    await bcryptjs.compare(admin.dataValues.senha_hash, req.session.admin.senha_hash)
-            .then((result) => {
-                if(!result){
-                    return res.json('render 404');
-                }
-            }); // PAREI AQUI
+    await adminRepository.findOne()
+    .then((result) => {
+        if(result.login != req.session.admin.login){ // verifico se as informações do BD coincidem.
+            res.json('redirecionando pra pag 404');
+        }
+    })
     next();
 }
