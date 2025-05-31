2c2778f617c9f4219110171709a80a7beed9e88a
Victor hugo <victr.hf@gmail.com>
2025-05-30 22:35:11 -0300

    add e-mail confirmation controller

--- src/controllers/cadastrar.js
+++ src/controllers/cadastrar.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
+const randomStringGenerator = require('random-string-generator');
 const UsuarioRepository = require('../models/usuario');
+const sgMail = require('@sendgrid/mail');
 const validator = require('validator');
+let userInfo = {};
+const authCode = randomStringGenerator(6);
+
 
 function index (req, res){
     res.json('sign up'); // aqui seria a renderização do EJS
@@ -19,26 +25,70 @@ async function store (req, res){
             }
         }).then((result) => {
             if(result) {
-                req.session.save();
-                res.json('e-mail já utilizado'); // se o já existir o e-mail no BD, isso sera retornado
+                res.json('e-mail já utilizado'); // se já existir o e-mail no BD, isso sera retornado
+            } else { // caso passe pela última validação, vou guardar em uma váriavel as informações que foram digitadas
+                req.session.firstStep = true;
+
+                userInfo.nome = req.body.nome;
+                userInfo.email = req.body.email;
+                userInfo.telefone = req.body.telefone;
+                userInfo.senha = req.body.senha;
             }
         });
-        
 
-        await UsuarioRepository.create({
-            nome: req.body.nome,
-            email: req.body.email,
-            telefone: req.body.telefone,
-            senha: req.body.senha
-        }).then((result) => {
-            req.session.save();
-            res.json(result); 
-            // res.redirect('/sign-in'); 
-        });
+
+        res.json('renderizo a página de confirmação de email');    
     }
     catch (error){
         res.json(error)
     }
 }
 
-module.exports = { index, store };
+function indexConfirmacao(req, res) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: userInfo.email,
+            from: 'solidariofacil@gmail.com',
+            subject: `CÓDIGO DE VERIFICAÇÃO FÁCIL SOLIDÁRIO`,
+            text: 
+            `
+                Seu código de verificação é: 
+    
+                \n \n \n
+    
+                <strong>${authCode}</strong>   
+
+            `
+        };
+        sgMail.send(msg);
+    
+    res.json(userInfo);
+}
+
+
+
+
+async function storeConfirmacao(req, res){
+
+    if(req.body.codigo != authCode){
+        res.json('código incorreto!!');
+    }
+
+    try {
+        await UsuarioRepository.create({
+            nome: userInfo.nome,
+            email: userInfo.email,
+            telefone: userInfo.telefone,
+            senha: userInfo.senha
+        }).then(() => {
+            res.json("conta criada!!"); 
+            req.session.firstStep = false;
+            // res.redirect('/sign-in'); 
+        });
+    } catch (error) {
+        res.json(error)
+    }
+    // colocar um req.session.firstStep = false; no final
+}
+
+module.exports = { index, store, indexConfirmacao, storeConfirmacao };
