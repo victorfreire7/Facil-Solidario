321ee4e7c488a5567a174d31f29edd31dbd036e6
Victor hugo <victr.hf@gmail.com>
2025-05-30 22:34:14 -0300

    add e-mail confirmation routes

--- src/routes/cadastrar.js
+++ src/routes/cadastrar.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const cadastrarController = require('../controllers/cadastrar');
+const firstStepMiddleware = require('../middlewares/secStepLoginRequired');
 
 router.get('/', cadastrarController.index);
 router.post('/', cadastrarController.store);
+
+router.get('/confirmacao', firstStepMiddleware, cadastrarController.indexConfirmacao);
+router.post('/confirmacao', firstStepMiddleware, cadastrarController.storeConfirmacao);
 
 
 module.exports = router;
