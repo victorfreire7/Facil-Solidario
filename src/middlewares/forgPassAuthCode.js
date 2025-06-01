54e132d9b217760082f7dc9c4734d0f6ecd2e62a
Victor hugo <victr.hf@gmail.com>
2025-06-01 00:17:11 -0300

    add functions middlewares

--- src/middlewares/forgPassAuthCode.js
+++ src/middlewares/forgPassAuthCode.js
@@ -1,8 +1,19 @@
-module.exports = (req, res, next) => {
-    if(!req.session.forgetPassowrdAuthCode){
+function emailValidator(req, res, next){
+    if(!req.session.forgetPassowrdAuthCode){ // sessao caso o código já tenha sido enviado
         res.redirect('renderizo a pag 404');
         res.redirect('/');
     }
 
     next();
 }
+
+function codeValidator(req, res, next){
+    if(!req.session.authCodeSucces || !req.session.forgetPassowrdAuthCode){ // sessao caso o código tenha sido enviado com sucesso.
+        res.redirect('renderizo a pag 404');
+        res.redirect('/');
+    }
+
+    next();
+}
+
+module.exports = { emailValidator, codeValidator }
