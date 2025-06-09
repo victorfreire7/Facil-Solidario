633502015b4b4cd9a7189e5f2d7339c8b1bb84e1
darkboat <rssgamer2017@gmail.com>
2025-06-09 10:53:29 -0300

    creating-creators-page-and-carrossel

--- js/script-home.js
+++ js/script-home.js
@@ -13,3 +13,30 @@ const checkbox = document.getElementById('menu-checkbox');
     }
   });
 
+
+  let currentIndex = 0; // Indice atual da imagem
+const slides = document.querySelector('.carrossel');
+const dots = document.querySelectorAll('.dot');
+
+// Função para atualizar o índice visual
+function updateIndex() {
+    // Remove a classe 'active' de todos os dots
+    dots.forEach(dot => dot.classList.remove('active'));
+    
+    // Adiciona a classe 'active' ao dot correspondente
+    dots[currentIndex].classList.add('active');
+}
+
+// Função para mudar a imagem automaticamente
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % dots.length; // Loop infinito
+    slides.style.transform = `translateX(-${currentIndex * 62}vw)`; // Muda a posição
+    updateIndex(); // Atualiza o índice
+}
+
+// Inicializar o carrossel e a troca automática de imagens
+setInterval(nextSlide, 3000); // Troca a cada 3 segundos
+
+// Atualiza o índice ao carregar
+updateIndex();
+
