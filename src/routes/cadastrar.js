2e57a27531e2ee38d3af5ddb72e239dc52361cec
Victor hugo <victr.hf@gmail.com>
2025-08-19 16:54:00 -0300

    minimal changes

--- src/routes/cadastrar.js
+++ src/routes/cadastrar.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const cadastrarController = require('../controllers/cadastrar');
-const firstStepMiddleware = require('../middlewares/firstStepLoginRequired');
+const stepMiddleware = require('../middlewares/stepLoginRequired');
 
 router.get('/', cadastrarController.index);
 router.post('/', cadastrarController.store);
 
-router.get('/confirmacao', firstStepMiddleware, cadastrarController.indexConfirm);
+router.get('/confirmacao', stepMiddleware.first, cadastrarController.indexConfirm);
+
+router.get('/code', stepMiddleware.first, cadastrarController.sendCode); // essa rota envia um código ao email do usuario
 
 // router.get('/confirmacao', /*firstStepMiddleware, TEMPORARIO REMOVIDO*/ cadastrarController.indexConfirmacao);
 // router.post('/confirmacao', /*firstStepMiddleware,*/ cadastrarController.storeConfirmacao);
