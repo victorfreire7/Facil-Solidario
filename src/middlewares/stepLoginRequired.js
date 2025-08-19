4fd0899bf1f405b6db30723b06523d1b2582ce66
Victor hugo <victr.hf@gmail.com>
2025-08-19 16:55:17 -0300

    change path

--- src/middlewares/stepLoginRequired.js
+++ src/middlewares/stepLoginRequired.js
@@ -1,8 +1,9 @@
-module.exports = (req, res, next) => {
+function first (req, res, next){
     if(!req.session.firstStep){
         res.status(404).render('404');
     } else {
         next();
     }
-
 }
+
+module.exports = { first }
