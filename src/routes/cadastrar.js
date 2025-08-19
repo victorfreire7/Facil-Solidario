0403546ad7f95db35780f19af24447ac836d3340
Victor hugo <victr.hf@gmail.com>
2025-08-19 13:01:37 -0300

    add firststep middleware

--- src/routes/cadastrar.js
+++ src/routes/cadastrar.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const cadastrarController = require('../controllers/cadastrar');
-const firstStepMiddleware = require('../middlewares/secStepLoginRequired');
+const firstStepMiddleware = require('../middlewares/firstStepLoginRequired');
 
 router.get('/', cadastrarController.index);
 router.post('/', cadastrarController.store);
 
-router.get('/confirmacao', cadastrarController.indexConfirm);
+router.get('/confirmacao', firstStepMiddleware, cadastrarController.indexConfirm);
 
 // router.get('/confirmacao', /*firstStepMiddleware, TEMPORARIO REMOVIDO*/ cadastrarController.indexConfirmacao);
 // router.post('/confirmacao', /*firstStepMiddleware,*/ cadastrarController.storeConfirmacao);
