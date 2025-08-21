31e0feffab7976f5db382a32a45d2fd391cbb143
etecAbh2023 <125657321+etecAbh2023@users.noreply.github.com>
2025-08-21 10:39:23 -0300

    correct email API

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -9,16 +9,16 @@ function store (req, res){
     try {
         sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         const msg = {
+            from: 'victr.hf@gmail.com',
             to: 'solidariofacil@gmail.com',
-            from: 'solidariofacil@gmail.com',
             subject: `nome: ${req.body.nome} email: ${req.body.email}`,
             text: req.body.texto,
         };
         sgMail.send(msg).then((result) => {
-            res.json('E-mail enviado com sucesso!'); 
+            res.redirect('/'); // FLASH MESSAGES AQUI... 
         });
     } catch (error) {
-       res.json(error) 
+       res.json(error); 
     }
 }
 
