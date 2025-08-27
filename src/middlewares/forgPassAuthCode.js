8386cedcef38d0ea8ef49b661a116597d602d6fb
Victor hugo <victr.hf@gmail.com>
2025-08-26 23:48:14 -0300

    remove unnecessary middleware

--- src/middlewares/forgPassAuthCode.js
+++ src/middlewares/forgPassAuthCode.js
@@ -1,21 +1,9 @@
-function emailValidator(req, res, next){
-    if(!req.session.forgetPasswordAuthCode){ // sessao caso o código já tenha sido enviado
-        res.status(404).render('404');
+function authCode(req, res, next){
+    if(!req.session.authCode){ // caso o codigo nao tenha sido enviado.
+        return res.status(404).render('404');
     }
 
     next();
 }
 
-function codeValidator(req, res, next){
-    if(!req.session.authCodeSucces){ // sessao caso o código tenha sido enviado com sucesso.
-        res.status(404).render('404');
-    }
-
-    if(!req.session.forgetPasswordAuthCode){ // sessao caso o código já tenha sido enviado
-        res.status(404).render('404');
-    }
-
-    next();
-}
-
-module.exports = { emailValidator, codeValidator }
+module.exports = { authCode }
