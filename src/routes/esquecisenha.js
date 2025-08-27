34c105656f46b4bde021ab3d3243e391c292dd63
Victor hugo <victr.hf@gmail.com>
2025-08-26 23:46:01 -0300

    correction of routes path

--- src/routes/esquecisenha.js
+++ src/routes/esquecisenha.js
@@ -6,10 +6,9 @@ const authCodeRequired = require('../middlewares/forgPassAuthCode');
 router.get('/', esquecisenhaController.index);
 router.post('/', esquecisenhaController.store);
 
-router.get('/authentication-code', authCodeRequired.emailValidator, esquecisenhaController.authCodeIndex);
-router.post('/authentication-code', authCodeRequired.emailValidator, esquecisenhaController.authCodeStore);
+router.get('/change-password', authCodeRequired.authCode, esquecisenhaController.changePassIndex);
+router.post('/change-password', authCodeRequired.authCode, esquecisenhaController.changePassUpdate)
 
-router.get('/authentication-code/change-password', authCodeRequired.codeValidator, esquecisenhaController.updatePasswordIndex);
-router.put('/authentication-code/change-password', authCodeRequired.codeValidator, esquecisenhaController.updatePasswordUpdate);
+router.get('/code', esquecisenhaController.sendCode)
 
 module.exports = router;
