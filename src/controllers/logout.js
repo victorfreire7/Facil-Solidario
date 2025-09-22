e9f5d68de79e3c7392d7fb71210531a7899e8e63
etecAbh2023 <125657321+etecAbh2023@users.noreply.github.com>
2025-09-22 09:39:14 -0300

    add success flash message

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -1,6 +1,6 @@
 function index (req, res){
     req.session.user = false;
-    req.flash('loginSucess', ['Logout realizado com sucesso!'])
+    req.flash('successMessage', ['Logout realizado com sucesso!'])
     res.redirect('/'); // FLASH MESSAGE AVISANDO QUE O LOGOUT FOI REALIZADO
 }
 
