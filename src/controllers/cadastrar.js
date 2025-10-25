3016ecb880848475f4cb8242d641d2a90ded9355
Victor hugo <victr.hf@gmail.com>
2025-10-25 00:10:54 -0300

    add password validator for user

--- src/controllers/cadastrar.js
+++ src/controllers/cadastrar.js
@@ -122,29 +122,81 @@ function indexPassword(req, res) {
     );
 }
 
+function passwordSeparator(arg){ // essa funçao separa todos os caracteres da string, para ser realizada uma separaçao de seus respectivos valores
+    const passwordArray = arg.split('');
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const data = {
+        special: 0,
+        upper: 0,
+        number: 0
+    }
+
+    for (const char of passwordArray) { //contem todas as caracteres da senha
+        for (const number of numbers) {
+            if(parseInt(char) === number){
+                data.number++;
+            }
+        }
+        if(char.charCodeAt() >= 65 && char.charCodeAt() <= 91){
+            data.upper++;
+        }
+        if(char.charCodeAt(0) >= 33 && char.charCodeAt(0) <= 47){
+            data.special++
+        }
+        if(char.charCodeAt(0) >= 58 && char.charCodeAt(0) <= 64){
+            data.special++
+        }
+        if(char.charCodeAt(0) >= 91 && char.charCodeAt(0) <= 96){
+            data.special++
+        }
+        if(char.charCodeAt(0) >= 123 && char.charCodeAt(0) <= 126){
+            data.special++
+        }
+    }
+
+    return data; // retorno os valores especiais da senha
+}
+
 async function storePassword(req, res) {
     try {
-        await UsuarioRepository.create({
-            nome: req.session.userInfo.nome ,
+        const password = passwordSeparator(req.body.senha);
+        if(req.body.senha !== req.body.confirmSenha){ // verifico se os dois inputs sao iguais
+            req.flash('errorMessage', ['Senhas não coincidem.']);
+            return res.redirect('/sign-up/password');
+        }  
+        if(req.body.senha.length < 8){ // verifico se a senha é maior do que o esperado
+            req.flash('errorMessage', ['Por favor, digite uma senha maior.']);
+            return res.redirect('/sign-up/password');
+        }
+        if(
+            password.number < 2 ||
+            password.special < 1 ||
+            password.upper < 1 
+        ){
+            req.flash('errorMessage', ['Por favor, cumpra os requisitos para uma senha segura.']);
+            return res.redirect('/sign-up/password');
+        }
+        await UsuarioRepository.create({ // caso o usuario passe por todo o validador, sera criado o login no BD
+            nome: req.session.userInfo.nome,
             email: req.session.userInfo.email,
             telefone: req.session.userInfo.telefone,
             senha: req.body.senha
         }).then(() => {
             req.session.firstStep = false;
             req.session.secondStep = false;
-            req.session.userInfo = false;
+            req.session.userInfo = false; // desativo todos os passos de sign-up ativos anteriormente, para acesso da rota desejada
             
             req.flash('successMessage', ['Conta criada com sucesso! Faça login para prosseguir.']);
             return res.redirect('/sign-in'); 
-        })
-
+        });
     } catch (error) {
         if(error.message == "Validation error"){
             req.flash('errorMessage', ['insira um E-mail e/ou Telefone válidos']);
             return res.redirect('/sign-up');
         } else {
+            console.log(error)
             req.flash('errorMessage', ['Um erro inesperado aconteceu! Tente novamente mais tarde.']);
-            return res.redirect('/sign-up');
+            return res.redirect('/sign-up/password');
         }
     }
 }
