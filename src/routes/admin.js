993aab2cf33614816032394e7d54a2034f198c78
Victor hugo <victr.hf@gmail.com>
2025-10-26 18:15:38 -0300

    add donation store route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,6 +7,10 @@ router.get("/", adminController.index);
 router.get("/:email", adminController.show);
 router.get("/:email/delete", adminController.deleteUser);
 
+router.get("/:email/create", adminController.showCreateDonation);
+router.post("/:email/create", adminController.storeDonation);
+
+
 router.get("/:email/:doacao", adminController.showDoacao);
 router.post("/:email/:doacao", adminController.update);
 router.get("/:email/:doacao/delete", adminController.deleteDoacao)
