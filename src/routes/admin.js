ede24e1edf327d659ad565cd7728ebdfa41f1baf
Victor hugo <victr.hf@gmail.com>
2025-10-26 15:04:56 -0300

    deleter route

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,8 +5,10 @@ const adminController = require('../controllers/admin');
 router.get("/", adminController.index);
 
 router.get("/:email", adminController.show);
+router.get("/:email/delete", adminController.deleteUser);
 
-router.get("/:email/:doacao", adminController.showDoacao)
+router.get("/:email/:doacao", adminController.showDoacao);
 router.post("/:email/:doacao", adminController.update);
+router.get("/:email/:doacao/delete", adminController.deleteDoacao)
 
 module.exports = router;
