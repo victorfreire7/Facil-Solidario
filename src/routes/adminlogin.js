d2cb2550bbe9afbe32626a798649077c9b08387e
Victor hugo <victr.hf@gmail.com>
2025-10-26 18:45:54 -0300

    remove sencode route

--- src/routes/adminlogin.js
+++ src/routes/adminlogin.js
@@ -5,6 +5,4 @@ const adminloginController = require('../controllers/adminlogin');
 router.get('/', adminloginController.index);
 router.post('/', adminloginController.login);
 
-router.get('/code', adminloginController.sendCode)
-
 module.exports = router;
