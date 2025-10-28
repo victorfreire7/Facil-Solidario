680f8feed328f1efb0be05f91e8ba38b03822524
Victor H. Freire <victr.hf@gmail.com>
2025-10-28 19:57:35 -0300

    change host listening

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require('./app');
 
-app.on('connection-established', () => { // ao a string de emissao ser ativa, o proximo método é capacitado de ser utilizado.
+app.on('connection-established', '0.0.0.0', () => { // ao a string de emissao ser ativa, o proximo método é capacitado de ser utilizado.
     app.listen(parseInt(process.env.PORT), () => {
         console.log(`Ouvindo na porta http://localhost:${process.env.PORT}`);
     });
