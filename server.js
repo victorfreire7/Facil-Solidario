d2125c6efee79282c84bbf79a707b7ff145dc605
Victor hugo <victr.hf@gmail.com>
2025-10-28 16:41:51 -0300

    correct sintax error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const app = require('./app');
 
 app.on('connection-established', () => { // ao a string de emissao ser ativa, o proximo método é capacitado de ser utilizado.
-    app.listen(port, () => {
-        console.log(`Ouvindo na porta http://localhost:${parseInt(process.env.PORT)}`);
+    app.listen(parseInt(process.env.PORT), () => {
+        console.log(`Ouvindo na porta http://localhost:${process.env.PORT}`);
     });
 });
