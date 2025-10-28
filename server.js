27be5437c80f790f0767a354f4ed4628803581d8
Victor hugo <victr.hf@gmail.com>
2025-10-28 16:37:47 -0300

    change PORT and move to .env file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require("dotenv").config();
 const app = require('./app');
-const port = 3030;
 
 app.on('connection-established', () => { // ao a string de emissao ser ativa, o proximo método é capacitado de ser utilizado.
     app.listen(port, () => {
-        console.log(`Ouvindo na porta http://localhost:${port}`);
+        console.log(`Ouvindo na porta http://localhost:${parseInt(process.env.PORT)}`);
     });
 });
