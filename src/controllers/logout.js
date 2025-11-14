2eededd901d7e9d172d4c0c3f34d7cc12f5e0592
etecAbh2023 <125657321+etecAbh2023@users.noreply.github.com>
2025-11-14 11:35:29 -0300

    update destroy session logic

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -1,5 +1,11 @@
 function index (req, res){
-    req.session.user = false;
+    req.session.destroy((error) => {
+        if(error){
+            req.flash('errorMessage', ['Um erro inesperado aconteceu! Tente novamente mais tarde.']);
+            return res.redirect('/');
+        }
+    })
+
     req.flash('successMessage', ['Logout realizado com sucesso!'])
     res.redirect('/'); // FLASH MESSAGE AVISANDO QUE O LOGOUT FOI REALIZADO
 }
