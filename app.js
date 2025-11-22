6fdf2a6232ae52cea6b15f151767e00e88f8dd70
victor <victr.hf@gmail.com>
2025-11-22 17:25:25 -0300

    minimal changes

--- app.js
+++ app.js
@@ -73,7 +73,6 @@ class App {
             resave: false,
             saveUninitialized: true,
             cookie: {
-                secure: true,
                 maxAge: 30*24*60*60*1000
             }
         }));
