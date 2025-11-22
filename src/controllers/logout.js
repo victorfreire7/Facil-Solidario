3604a987130608beae9612e02520c3a4a6bbfa8d
victor <victr.hf@gmail.com>
2025-11-22 00:51:46 -0300

    add trycatch

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -1,8 +1,13 @@
 function index (req, res){
-    req.session.user = null;
-    req.session.admin = null;
-    req.flash('successMessage', ['Logout realizado com sucesso!']);
-    return res.redirect('/');
+    try {
+        req.session.user = null;
+        req.session.admin = null;
+        req.flash('successMessage', ['Logout realizado com sucesso!']);
+        return res.redirect('/');
+    } catch (error) {
+        req.flash('errorMessage', ['Um erro inesperado aconteceu! Tente novamente mais tarde.']);
+        return res.redirect('/sign-up/password');
+    }
 }
 
 module.exports = { index }
