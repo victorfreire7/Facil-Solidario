b48d884584e16a8cff0f43cef9149813243e9627
victor <victr.hf@gmail.com>
2025-11-22 00:53:51 -0300

    minimal details

--- src/controllers/logout.js
+++ src/controllers/logout.js
@@ -1,12 +1,7 @@
 function index (req, res){
-    try {
         req.session.user = null;
         req.session.admin = null;
         return res.redirect('/');
-    } catch (error) {
-        req.flash('errorMessage', ['Um erro inesperado aconteceu! Tente novamente mais tarde.']);
-        return res.redirect('/sign-up/password');
-    }
 }
 
 module.exports = { index }
