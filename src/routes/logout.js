fd60f5f941e23ebe3d5723a399b98644c7b66cce
victor <victr.hf@gmail.com>
2025-11-22 01:09:49 -0300

    test

--- src/routes/logout.js
+++ src/routes/logout.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    req.session.user = null;
-    req.session.admin = null;
+    // req.session.user = null;
+    // req.session.admin = null;
+
+    req.flash('successMessage', ['Logout realizado com sucesso!']);
     res.redirect('/');
 });
 
